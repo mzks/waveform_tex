1a3b07dfbc8f466bfda4426b96709c434282638f
mzks <mz@stu.kobe-u.ac.jp>
2017-01-26 22:20:49 +0900

    add baseline

--- Loop.C
+++ Loop.C
@@ -3,14 +3,18 @@
 // 2017 Jan. 26
 
 void Loop(){
+
+	const Double_t THRESHOLD = -1.0e-3; //Volt
+
+
 	//Init
-	TString header = "./data/aho2_";
+	TString header = "./data/coin_";
 	TString footer = "_ch1.root";
 	stringstream ss;
 	Int_t fileNumber=0;
 	ss << header << fileNumber << footer;
 
-	TH1D* histCharge = new TH1D("charge","charge",10000,-1.0e-3,1.0e-3);
+	TH1D* histCharge = new TH1D("charge","charge",1000,-0.2e-9,0.1e-9);
 
 	// Loop
 	while(TFile* f1 = TFile::Open(ss.str().c_str())){
@@ -19,17 +23,28 @@ void Loop(){
 		Double_t* x = g1->GetX();
 		Double_t* y = g1->GetY();
 
+
+		//define baseline
+		Double_t baseline;
+		for(int i=0;i<1000;++i){
+			baseline += y[i];
+		}
+		baseline /= 1000.0;
+		
 		Double_t integral = 0;
 		for(int i=0;i<10000;++i){
-			integral += y[i];
+			if((y[i]-baseline)<THRESHOLD) integral += y[i]-baseline;
 		}
-		histCharge->Fill(integral *= 4.0e-10);
 
+		histCharge->Fill(integral *= (4.0e-10/50.0) ); //50.0 Ohm
+
+		// prepare for next loop
 		ss.str("");
 		ss.clear(stringstream::goodbit);
 		++fileNumber;
 		ss << header << fileNumber << footer;
 	}
+	histCharge->SetTitle("Charge;C;count");
 	histCharge->Draw();
 
 }
