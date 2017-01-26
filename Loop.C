bdb27460fcf55a4d34159fa8cc0140ecdc5cf9f5
mzks <mz@stu.kobe-u.ac.jp>
2017-01-26 21:38:08 +0900

    add integral code

--- Loop.C
+++ Loop.C
@@ -4,24 +4,32 @@
 
 void Loop(){
 	//Init
-	TString header = "./data/aho1_";
+	TString header = "./data/aho2_";
 	TString footer = "_ch1.root";
 	stringstream ss;
 	Int_t fileNumber=0;
 	ss << header << fileNumber << footer;
 
+	TH1D* histCharge = new TH1D("charge","charge",10000,-1.0e-3,1.0e-3);
+
+	// Loop
 	while(TFile* f1 = TFile::Open(ss.str().c_str())){
 		TGraph* g1 = dynamic_cast<TGraph*>(f1->Get("graph"));
 
 		Double_t* x = g1->GetX();
 		Double_t* y = g1->GetY();
 
-		cout << x[100]  << ";"<< y[100] << endl;
+		Double_t integral = 0;
+		for(int i=0;i<10000;++i){
+			integral += y[i];
+		}
+		histCharge->Fill(integral *= 4.0e-10);
 
 		ss.str("");
 		ss.clear(stringstream::goodbit);
 		++fileNumber;
 		ss << header << fileNumber << footer;
 	}
+	histCharge->Draw();
 
 }
