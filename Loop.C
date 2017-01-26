04d041413a75d80723ae30301ece30aae460ceda
mzks <mz@stu.kobe-u.ac.jp>
2017-01-26 21:20:54 +0900

    Complete loop

---
+++ Loop.C
@@ -0,0 +1,27 @@
+// Loop.C -- root macro
+// Author : Mizukoshi Keita
+// 2017 Jan. 26
+
+void Loop(){
+	//Init
+	TString header = "./data/aho1_";
+	TString footer = "_ch1.root";
+	stringstream ss;
+	Int_t fileNumber=0;
+	ss << header << fileNumber << footer;
+
+	while(TFile* f1 = TFile::Open(ss.str().c_str())){
+		TGraph* g1 = dynamic_cast<TGraph*>(f1->Get("graph"));
+
+		Double_t* x = g1->GetX();
+		Double_t* y = g1->GetY();
+
+		cout << x[100]  << ";"<< y[100] << endl;
+
+		ss.str("");
+		ss.clear(stringstream::goodbit);
+		++fileNumber;
+		ss << header << fileNumber << footer;
+	}
+
+}
